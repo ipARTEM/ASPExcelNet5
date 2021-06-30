454a14f8e4beab5149ca59cfa09e22cb623508a1
Artem <a.khimin@yandex.ru>
2021-06-30 21:48:53 +0300

    Добавьте файлы проекта.

---
+++ Models/DomainModels.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPExcelNet5.Models
+{
+    public class PricePosition
+    {
+        //Неисправность
+        public string Problem { get; set; }
+
+        //Стоимость ремонта
+        public string Price { get; set; }
+    }
+
+    public class PhoneModel
+    {
+        public PhoneModel()
+        {
+            PricePositions = new List<PricePosition>();
+        }
+
+        //Название модели телефона
+        public string Title { get; set; }
+
+        public List<PricePosition> PricePositions { get; set; }
+    }
+
+    public class PhoneBrand
+    {
+        public PhoneBrand()
+        {
+            PhoneModels = new List<PhoneModel>();
+        }
+
+        //Название бренда
+        public string Title { get; set; }
+
+        public List<PhoneModel> PhoneModels { get; set; }
+    }
+}
---
+++ Models/ViewModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPExcelNet5.Models
+{
+    public class PriceViewModel
+    {
+        public PriceViewModel()
+        {
+            PhoneBrands = new List<PhoneBrand>();
+        }
+
+        public List<PhoneBrand> PhoneBrands { get; set; }
+
+        //кол-во ошибок при импорте
+        public int ErrorsTotal { get; set; }
+    }
+}
